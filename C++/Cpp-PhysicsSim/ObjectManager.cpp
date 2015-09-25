3f639039f937ecc523a73a6d7db9801dad36fedb
Alex H <hehe3301@gmail.com>
2015-09-25 10:08:58 -0400

    Created ObjectManager
    Created the ObjectManager which will be used to let other objects
    calculate the forces on them

---
+++ C++/Cpp-PhysicsSim/ObjectManager.cpp
@@ -0,0 +1,18 @@
+/* 
+ * File:   ObjectManager.cpp
+ * Author: Alexander Habermann
+ * 
+ * Created on September 25, 2015, 10:02 AM
+ */
+
+#include "ObjectManager.h"
+
+ObjectManager::ObjectManager() {
+}
+
+ObjectManager::ObjectManager(const ObjectManager& orig) {
+}
+
+ObjectManager::~ObjectManager() {
+}
+
---
+++ C++/Cpp-PhysicsSim/ObjectManager.h
@@ -0,0 +1,25 @@
+/* 
+ * File:   ObjectManager.h
+ * Author: Alexander Habermann
+ *
+ * Created on September 25, 2015, 10:02 AM
+ */
+
+#ifndef OBJECTMANAGER_H
+#define	OBJECTMANAGER_H
+
+class ObjectManager {
+public:
+    ObjectManager * Instance();
+    void InstanceExit();
+private:
+    int objCount;
+    ObjectManager * mpObjectManager;
+    ObjectManager();
+    ObjectManager(const ObjectManager& orig);
+    virtual ~ObjectManager();
+
+};
+
+#endif	/* OBJECTMANAGER_H */
+
